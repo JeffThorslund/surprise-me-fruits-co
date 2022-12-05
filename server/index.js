904e805cc61956b3ced064a9396572b479ee97dd
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-12-05 09:49:49 -0500

    Introduce server middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
