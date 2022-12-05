a6763cd2044ad4449542d4d8bb3c842ab9147358
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-12-05 11:02:59 -0500

    Separate customer table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,34 +24,40 @@ function App() {
 
   return (
     <Grommet theme={theme}>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableCell scope="col" border="bottom">
-              Name
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              Flavor
-            </TableCell>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell scope="row">
-              <strong>Eric</strong>
-            </TableCell>
-            <TableCell>Coconut</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell scope="row">
-              <strong>Chris</strong>
-            </TableCell>
-            <TableCell>Watermelon</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <CustomerTable />
     </Grommet>
   );
 }
+
+const CustomerTable = () => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableCell scope="col" border="bottom">
+            Name
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            Flavor
+          </TableCell>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell scope="row">
+            <strong>Eric</strong>
+          </TableCell>
+          <TableCell>Coconut</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell scope="row">
+            <strong>Chris</strong>
+          </TableCell>
+          <TableCell>Watermelon</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
 
 export default App;
