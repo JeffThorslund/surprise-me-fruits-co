dba90a92caaccaed5fa91ce9ecd49a736a6cd61c
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-12-05 10:12:26 -0500

    Add requests as middleware to the router

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const poolErrorHandler = require("./helpers/poolErrorHandler")
+const router = require('./routes/index')
 const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,73 +13,12 @@ app.use(
     })
 );
 
+app.use("/api", router);
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-app.get("/products", (req, res) => {
-    poolErrorHandler("SELECT * from product", (result) => {
-        res.json(result.rows);
-    });
-})
 
-app.get("/customers", (req, res) => {
-    poolErrorHandler("SELECT * from customer", (result) => {
-        res.json(result.rows);
-    });
-});
-
-// UPDATE
-app.put("/global-fruit-limit", (req, res) => {
-    const { customer_id, min, max } = req.body;
-    handlePoolQuery(
-        `UPDATE customer SET min= ${min}, max= ${max} WHERE id = ${customer_id}`,
-        (result) => {
-            res.json(result.rows);
-        }
-    );
-});
-
-// Specific Fruit Parameters CRUD
-
-// CREATE
-app.post("/specific-fruit-limit", (req, res) => {
-    const { customer_id, product_id } = req.body;
-    handlePoolQuery(
-        `INSERT INTO specific_fruit_limit(customer_id, product_id) VALUES (${customer_id}, ${product_id})`,
-        (result) => {
-            res.json(result.rows);
-        }
-    );
-});
-
-// READ
-app.get("/specific-fruit-limit", (req, res) => {
-    handlePoolQuery("SELECT * from specific_fruit_limit", (result) => {
-        res.json(result.rows);
-    });
-});
-
-// UPDATE
-app.put("/specific-fruit-limit", (req, res) => {
-    const { specific_fruit_limit_id, min, max } = req.body;
-    handlePoolQuery(
-        `UPDATE specific_fruit_limit SET min= ${min}, max= ${max} WHERE id = ${specific_fruit_limit_id}`,
-        (result) => {
-            res.json(result.rows);
-        }
-    );
-});
-
-// DELETE
-app.delete("/specific-fruit-limit", (req, res) => {
-    const { specific_fruit_limit_id } = req.body;
-    handlePoolQuery(
-        `DELETE FROM specific_fruit_limit WHERE id = ${specific_fruit_limit_id};`,
-        (result) => {
-            res.json(result.rows);
-        }
-    );
-});
 
 
