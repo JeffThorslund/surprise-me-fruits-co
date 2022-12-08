98d82cdc8cfc8cc4432bf30b4430f7cec5268378
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-12-08 10:25:05 -0500

    bugfix: define explicit hostnames per environment

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 const Pool = require("pg").Pool;
 
+const databaseHost = process.env.DATABASE_HOST || "localhost";
+
+console.log(databaseHost);
+
 const pool = new Pool({
-  connectionString: "postgres://postgres:postgres@database:5432/database",
+  connectionString: `postgres://postgres:postgres@${databaseHost}:5432/database`,
 });
 
 module.exports = pool;
